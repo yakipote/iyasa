d6127929dd154597a2b284e13d17e15c364c85a8
yakipote <yakipote@redimpulz.com>
2015-06-07 20:21:38 +0900

    first commit

---
+++ iyasa.rb
@@ -0,0 +1,8 @@
+#!/usr/local/bin/ruby
+path = File.expand_path(File.readlink(__FILE__))
+pathArray = path.split('/')
+pathArray.pop
+
+array = [1,2,3,4,5,6]
+puts `ls #{ARGV.join(' ')}`
+system("afplay #{pathArray.join('/')}/#{array.sample}.mp3 &")
