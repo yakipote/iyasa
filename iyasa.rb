7277b294d05c890211b5f3ce7685f36461a9ab11
yakipote <yakipote@redimpulz.com>
2015-06-11 12:29:47 +0900

    rmに変更

--- iyasa.rb
+++ iyasa.rb
@@ -4,5 +4,5 @@ pathArray = path.split('/')
 pathArray.pop
 
 array = [1,2,3,4,5,6]
-puts `ls #{ARGV.join(' ')}`
+puts `rm #{ARGV.join(' ')}`
 system("afplay #{pathArray.join('/')}/#{array.sample}.mp3 &")
